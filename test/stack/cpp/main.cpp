c7a1cc962f45ff92b75d2b7b996c2b9c9be2851f
alishazin <78612608+alishazin@users.noreply.github.com>
2023-08-24 00:24:33 +0530

    completed stack in cpp

---
+++ test/stack/cpp/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+class StackOverflow {};
+class StackUnderflow {};
+class StackEmpty {};
+
+class Stack {
+
+    int _size;
+    int top;
+    int *array;
+
+    public:
+
+        Stack(int _size) {
+            this->_size = _size;
+            this->top = -1;
+
+            this->array = new int[_size];
+        }
+
+        int isEmpty() {
+            if (this->top == -1) return 1;
+            return 0;
+        }        
+
+        int isFull() {
+            if (this->top == this->_size - 1) return 1;
+            return 0;
+        }  
+
+        int size() {
+            return this->_size;
+        }      
+
+        void push(int item) {
+            
+            if (this->isFull()) {
+                throw StackOverflow();
+            } 
+
+            this->top++;
+            this->array[this->top] = item;
+        }
+
+        int pop() {
+            
+            if (this->isEmpty()) {
+                throw StackUnderflow();
+            }
+            
+            this->top--;
+            return this->array[this->top + 1];
+        }
+
+        void display() {
+            
+            if (this->isEmpty()) {
+                throw StackEmpty();
+            }
+
+            cout << endl;
+            for (int i=this->top; i>-1; i--) {
+                if (i == this->top) {
+                    cout << this->array[i] << " <-- Top" << endl;
+                } else {
+                    cout << this->array[i] << endl;
+                }
+            }
+            cout << "Size = " << this->_size << endl << endl; 
+        }
+
+        int peek() {
+
+            if (this->isEmpty()) {
+                throw StackEmpty();
+            }
+            
+            return this->array[this->top];
+        }
+
+};
+
+ostream& operator<<(ostream &out, Stack obj) {
+    obj.display();
+    return out;
+}
+
+int main() {
+
+    Stack stack = Stack(5);
+    
+    stack.push(11);
+    stack.push(14);
+    stack.push(17);
+    stack.push(21);
+    stack.push(23);
+
+    stack.display();
+
+    stack.pop();
+    cout << stack.pop() << endl;
+
+    cout << stack.peek() << endl;
+    cout << stack.size() << endl;
+
+    cout << stack << endl;
+
+    return 0;
+}
